bdab5f4afdd1356a57455108054cd372634cdcb3
tkhamokova <tkhamokova@INGOS-EDU017>
2019-07-26 12:36:49 +0300

    + текст постов и имена пользователей подгружаются с джейсонплейсхолдера ура!

--- script/readmore.js
+++ script/readmore.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let btnText1 = document.querySelectorAll(".myBtn");
 for (let i = 0; i < btnText1.length; i++) {
 		btnText1[i].addEventListener('click', function(e){ myFunction(e) }, false);
@@ -24,30 +23,3 @@ for (let i = 0; i < btnText1.length; i++) {
 		}
 	}
 	
-=======
-var btnText1 = document.querySelectorAll(".myBtn");
-		for (i=0; i<btnText1.length; i++){
-		btnText1[i].addEventListener('click', function(e){ myFunction(e) }, false);
-    }
-	
-	function myFunction(e) {
-		
-		var target = e.target;
-		var novast = target.parentNode;
-		
-		var moreText = novast.querySelector(".more");
-		var dots = novast.querySelector(".dots");
-		var btnText = novast.querySelector(".myBtn");
-
-		if (dots.style.display === "none") {
-		dots.style.display = "inline";
-		btnText.innerHTML = "Read more"; 
-		moreText.style.display = "none";
-		} else {
-		dots.style.display = "none";
-		btnText.innerHTML = "Read less"; 
-		moreText.style.display = "inline";
-		}
-	}
-	
->>>>>>> e2bef05be5ccbf20f899d41bbfe09b38a138976f
