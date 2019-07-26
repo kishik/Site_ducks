ca589a44d1f79bfde6f18c368bc8a4f7e7188cb0
Роман Ручин <ruchin@corp.ingos.ru>
2019-07-26 13:00:58 +0300

    исправления

--- script/readmore.js
+++ script/readmore.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 var btnText1 = document.querySelectorAll(".myBtn");
 		for (i=0; i<btnText1.length; i++){
@@ -24,30 +23,3 @@ var btnText1 = document.querySelectorAll(".myBtn");
 		moreText.style.display = "inline";
 		}
 	}
-=======
-let btnText1 = document.querySelectorAll(".myBtn");
-for (let i = 0; i < btnText1.length; i++) {
-		btnText1[i].addEventListener('click', function(e){ myFunction(e) }, false);
-    }
-	
-	function myFunction(e) {
-
-		let target = e.target;
-		let novast = target.parentNode;
-
-		let moreText = novast.querySelector(".more");
-		let dots = novast.querySelector(".dots");
-		let btnText = novast.querySelector(".myBtn");
-
-		if (dots.style.display === "none") {
-		dots.style.display = "inline";
-		btnText.innerHTML = "Read more"; 
-		moreText.style.display = "none";
-		} else {
-		dots.style.display = "none";
-		btnText.innerHTML = "Read less"; 
-		moreText.style.display = "inline";
-		}
-	}
-	
->>>>>>> bdab5f4afdd1356a57455108054cd372634cdcb3
