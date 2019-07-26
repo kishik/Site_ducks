fb1e25dcb21246b82bad2b56bd5f27eaf8b303f6
Maxim <kishik.maxim@gmail.com>
2019-07-26 18:35:14 +0300

    26_evening

--- script/readmore.js
+++ script/readmore.js
@@ -1,17 +1,17 @@
 
-var btnText1 = document.querySelectorAll(".myBtn");
+let btnText1 = document.querySelectorAll(".myBtn");
 		for (i=0; i<btnText1.length; i++){
 		btnText1[i].addEventListener('click', function(e){ myFunction(e) }, false);
     }
 	
 	function myFunction(e) {
 		
-		var target = e.target;
-		var novast = target.parentNode;
+		let target = e.target;
+		let novast = target.parentNode;
 		
-		var moreText = novast.querySelector(".more");
-		var dots = novast.querySelector(".dots");
-		var btnText = novast.querySelector(".myBtn");
+		let moreText = novast.querySelector(".more");
+		let dots = novast.querySelector(".dots");
+		let btnText = novast.querySelector(".myBtn");
 
 		if (dots.style.display === "none") {
 		dots.style.display = "inline";
