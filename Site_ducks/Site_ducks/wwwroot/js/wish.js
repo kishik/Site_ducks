1a13b81029830384f01ad68d9854753fe54f1e24
tkhamokova <tkhamokova@INGOS-EDU017>
2019-08-14 13:53:17 +0300

    newka

--- Site_ducks/Site_ducks/wwwroot/js/wish.js
+++ Site_ducks/Site_ducks/wwwroot/js/wish.js
@@ -1,4 +1,20 @@
-﻿function wishh() {
-    document.getElementClassName("wish").src = "http://placekitten.com/100/100";
+﻿
+function wishh() {
+    fetch("/home/getJSON").then(res => res.json()).then(res => publish(res));
+
+
+    function publish(res) {
+
+        let img = document.getElementById('wish');
+        img.width = 200;
+        img.height = 211;
+        let rand = 1 + Math.random() * (15 + 1 - 1);
+        let x = Math.floor(rand);
+
+        console.log(res[x].Pic);
+        img.innerHTML = '<img src=' + res[x].Pic + ' />';
+    }
 
 }
+
+
