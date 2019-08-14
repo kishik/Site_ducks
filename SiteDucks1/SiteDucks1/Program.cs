78de84e137c1dad781e8a036de4354cfe5281fe1
Evgeny <Evgeny.Sdobnov@ingos.ru>
2019-08-14 15:24:41 +0300

    Fix

--- SiteDucks1/SiteDucks1/Program.cs
+++ SiteDucks1/SiteDucks1/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Site_ducks
+namespace SiteDucks1
 {
     public class Program
     {
