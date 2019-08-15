d0b32af947e6f5b0fca20a0467207c881151f770
Evgeny <Evgeny.Sdobnov@ingos.ru>
2019-08-15 19:48:21 +0300

    My com

--- Site_ducks/wwwroot/js/takeInformatiomAboutMe.js
+++ Site_ducks/wwwroot/js/takeInformatiomAboutMe.js
@@ -2,10 +2,19 @@ function TakeInformation() {
     let requestURL = "https://" + window.location.host + "/Home/TakeMyInform";
     fetch(requestURL)
         .then(response => response.json())
-        .then(inform => );
+        .then(inform => AddInformationOnPage(inform));
 
 }
 function AddInformationOnPage(inform) {
-    document.querySelectorAll(".MyPhoto");
+    let allPhotos = document.querySelectorAll(".MyPhoto");
+    let allNames = document.querySelector(".MyName")
+    let allLinks = document.querySelector(".MyLink")
+    console.log(allNames);
+    for (let i = 0; i < allPhotos.length; i++) {
+        allPhotos[i].innerHTML = '<img src=' + inform.Photo + '  class="pic_with_text_near dark-change-border" />';
+    }
+    for (let i = 0; i < allNames.length; i++) {
+        allNames[i].innerText = inform.Email;
+    }
 
 }
