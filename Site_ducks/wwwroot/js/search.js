80531298902c300de80ea80c99b1430c08e8fd33
kishik <kishik.maxim@gmail.com>
2019-08-16 07:21:29 +0300

    Утро

--- Site_ducks/wwwroot/js/search.js
+++ Site_ducks/wwwroot/js/search.js
@@ -9,6 +9,26 @@ function search() {
     let text = document.getElementById("textSearch").value;
     let requestURL = "https://" + window.location.host + "/Home/Search";
     console.log(text);
+    let clonedNode = document.getElementById("example_1").cloneNode(true);
+    let AllText = clonedNode.querySelector("#example_6");
+    
+    searchDelete();
+    function AddNewsOnPage1(news) {
+        if ((news.length != 0)) {
+            for (let i = 0; i < news.length; i++) {
+                
+                console.log(news[i].Text);
+                
+                AllText.innerHTML = news[i].Text;
+                document.getElementById("example_0").appendChild(clonedNode);
+                clonedNode = document.getElementById("example_1").cloneNode(true);
+                AllText = clonedNode.querySelector("#example_6");
+                
+            }
+            TakeInformation();
+        }
+    }
+    
     //searchDelete();
     fetch(requestURL, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
@@ -19,6 +39,6 @@ function search() {
         }
     })
         .then(response => response.json())
-    .then(news => AddNewsOnPage(news));
+    .then(news => AddNewsOnPage1(news));
     
 }
